c2861bfc56056e1a564fd2f8830046a6d63e4f04
Ajinkya Rode <ajinkya.rode93@gmail.com>
2025-03-08 17:34:04 -0600

    Implemented queue using arraylist

--- QueueUsingArrayList.java
+++ QueueUsingArrayList.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class QueueUsingArrayList {
-    public static void main(String args[]){
+    public static void main(String[] args){
         ArrayList<Integer> queue = new ArrayList<>();
 
         //Enqueue
